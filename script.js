d506ae33e9d565d234b404e951e72337cc70d454
Shubh Krishna Shrivastava <118735729+ShubhKrishna15@users.noreply.github.com>
2023-05-25 10:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function updateClock(){
+	let p = document.getElementById("timer")
+	let currentTime = new Date();
+	let output = currentTime.getMonth()+"/"+currentTime.getDate()+"/"+currentTime.getFullYear()+", "+
+		currentTime.getHours()+":"+currentTime.getMinutes()+":"+currentTime.getSeconds();
+    p.innerHTML = output ;
+	setTimeout(updateClock,1000);
+}
+updateClock()
+
